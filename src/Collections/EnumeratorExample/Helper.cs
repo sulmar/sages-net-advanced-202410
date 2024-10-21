7135a5588626acfa04afee1f4b1da9fb39cc31f4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-21 11:11:49 +0200

    Dodanie przykładu iteracji po dniach tygodnia

---
+++ src/Collections/EnumeratorExample/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratorExample;
+
+internal class Helper
+{
+    public static IEnumerable<string> GetWeekDays()
+    {
+        return new List<string>
+        {
+            "Poniedziałek",
+            "Wtorek",
+            "Środa",
+            "Czwartek",
+            "Piątek",
+            "Sobota",
+            "Niedziela"
+        };
+    }
+}
--- src/Collections/EnumeratorExample/Program.cs
+++ src/Collections/EnumeratorExample/Program.cs
@@ -1,5 +1,34 @@
 ﻿
+using EnumeratorExample;
+using System.Collections.ObjectModel;
+
 Console.WriteLine("Hello, Enumerator!");
 
 
-List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+IEnumerable<string> weekDays = Helper.GetWeekDays();
+
+foreach (string weekDay in weekDays)
+{
+    Console.WriteLine(weekDay);
+
+    if (weekDay == "Środa")
+    {
+        break;
+    }
+}
+
+return;
+
+ICollection<int> numbers = new Collection<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+
+//for (int i = 0; i < numbers.Count; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+
