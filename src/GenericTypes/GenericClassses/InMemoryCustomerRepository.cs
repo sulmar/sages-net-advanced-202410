ca44939224c6dd53e6251c2f0341916bcc9d87cd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-21 15:37:17 +0200

    Dodanie klasy generycznej

--- src/GenericTypes/GenericClassses/InMemoryCustomerRepository.cs
+++ src/GenericTypes/GenericClassses/InMemoryCustomerRepository.cs
@@ -2,29 +2,47 @@
 
 #region Models
 
-public class Customer
+public abstract class Base
 {
-    public int CustomerId { get; set; }
+  
+}
+
+public abstract class BaseEntity : Base
+{
+    public int Id { get; set; }
+}
+
+public class Customer : BaseEntity
+{
+   
     public required string Name { get; set; }
 
     public override string ToString()
     {
-        return $"CustomerId: {CustomerId} Name: {Name}";
+        return $"CustomerId: {Id} Name: {Name}";
     }
 }
 
-public class Account
+public class Account : BaseEntity
 {
-    public int AccountId { get; set; }
     public required string Number { get; set; }
     public decimal Balance { get; set; }
     public override string ToString()
     {
-        return $"AccountId: {AccountId} Number: {Number} Balance: {Balance}";
+        return $"AccountId: {Id} Number: {Number} Balance: {Balance}";
     }
 }
 
 #endregion
+
+// Szablon - interfejs ugólniony (generyczny)
+public interface IEntityRepository<TEntity>     // TEntity - typ encji
+    where TEntity : BaseEntity                 // reguła (constraint)
+{
+    IEnumerable<TEntity> GetAll();
+    TEntity Get(int id);
+    void Add(TEntity entity);
+}
 
 public class InMemoryCustomerRepository
 {
@@ -32,32 +50,69 @@ public class InMemoryCustomerRepository
 
     public InMemoryCustomerRepository(IEnumerable<Customer> customers)
     {
-        _customers = customers.ToDictionary(p => p.CustomerId);
+        _customers = customers.ToDictionary(p => p.Id);
     }
 
-    public void AddCustomer(Customer customer)
+    public void Add(Customer customer)
     {
         var id = _customers.Max(p => p.Key);
 
-        customer.CustomerId = ++id;
+        customer.Id = ++id;
 
-        _customers.Add(customer.CustomerId, customer);
+        _customers.Add(customer.Id, customer);
     }
 
-    public Customer GetCustomer(int id)
+    public Customer Get(int id)
     {
         return _customers[id];
     }
 
-
-    public IEnumerable<Customer> GetAllCustomers()
+    public IEnumerable<Customer> GetAll()
     {
         return _customers.Values;
     }
 }
 
 
-// TODO: Create InMemoryAccountRepository
-public class InMemoryAccountRepository
-{ 
+public class InMemoryAccountRepository : InMemoryEntityRepository<Account>, IAccountRepository
+{
+    public InMemoryAccountRepository(IEnumerable<Account> entities) : base(entities)
+    {
+    }
+}
+
+public interface IAccountRepository : IEntityRepository<Account>
+{
+
+}
+
+// Szablon klasy (klasa generyczna)
+public class InMemoryEntityRepository<TEntity> : IEntityRepository<TEntity>
+    where TEntity : BaseEntity
+{
+    protected readonly IDictionary<int, TEntity> _entities;
+
+    public InMemoryEntityRepository(IEnumerable<TEntity> entities)
+    {
+        _entities = entities.ToDictionary(p => p.Id);
+    }
+
+    public virtual void Add(TEntity entity)
+    {
+        var id = _entities.Max(p => p.Key);
+
+        entity.Id = ++id;
+
+        _entities.Add(entity.Id, entity);
+    }
+
+    public virtual TEntity Get(int id)
+    {
+        return _entities[id];
+    }
+
+    public virtual IEnumerable<TEntity> GetAll()
+    {
+        return _entities.Values;
+    }
 }
