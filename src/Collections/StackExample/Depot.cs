dd38b21c9bfb84c45741693cbdd24821d06473b0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-21 12:42:47 +0200

    Dodanie przykładu stosu

--- src/Collections/StackExample/Depot.cs
+++ src/Collections/StackExample/Depot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,25 @@ namespace StackExample;
 
 internal class Depot
 {
+    private IDictionary<string, Stack<string>> _places = new Dictionary<string, Stack<string>>();
+
     public void AddItem(string item, string place)
     {
-        throw new NotImplementedException();
+        Stack<string> stack;
+
+        if (_places.ContainsKey(place))
+        {
+            stack = _places[place];          
+        }
+        else
+        {
+            stack = new Stack<string>();
+            _places.Add(place, stack);
+            
+        }
+
+        stack.Push(item);
+
     }
 
     public string SendItem(string place)
@@ -20,6 +37,16 @@ internal class Depot
 
     public void DisplayDepotContents()
     {
-        throw new NotImplementedException();
+        foreach (var place in _places)
+        {
+            Console.WriteLine(place.Key);
+
+            var stack = place.Value;
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
