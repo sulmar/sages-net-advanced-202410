96b3891d4a0d8ccc40e3f1ae44f2d9a0cc575540
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-21 16:32:59 +0200

    Dodanie przykładu Extension Methods

---
+++ src/ExtensionMethods/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, Extension Methods!");
+
+
+if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday || DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
+{
+    Console.WriteLine("Już weekend!");
+}
+else
+{
+    Console.WriteLine("Dzień roboczy.");
+}
