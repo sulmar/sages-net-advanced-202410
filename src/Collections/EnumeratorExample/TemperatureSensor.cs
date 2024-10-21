b8c7697c0653ff17044e910f881390636adf7756
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-21 11:54:25 +0200

    Dodanie przykładu czujnika

---
+++ src/Collections/EnumeratorExample/TemperatureSensor.cs
@@ -0,0 +1,23 @@
+﻿namespace EnumeratorExample;
+
+
+public interface ISensor
+{
+    IEnumerable<float> GetValues();
+}
+
+public class TemperatureSensor : ISensor
+{
+    public IEnumerable<float> GetValues()
+    {
+        Random random = new Random();
+
+        while (true)
+        {
+            // TODO: odczytaj wartość temperatury z czujnika
+            float temperature = random.NextSingle() * 100;
+
+            yield return temperature;
+        }
+    }
+}
