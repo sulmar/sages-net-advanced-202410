f687c31b436cf0e207587d62c02a0ff24f08239e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-22 16:37:06 +0200

    Dodanie Fluent Api

--- src/MethodChaining/FluentApi/Phone.cs
+++ src/MethodChaining/FluentApi/Phone.cs
@@ -2,14 +2,14 @@
 
 public class Phone
 {
-    public void Call(string from, string to, string subject)
-    {
-        Console.WriteLine($"Calling from {from} to {to} with subject {subject}");
-    }
-
     public void Call(string from, string to)
     {
         Console.WriteLine($"Calling from {from} to {to}");
+    }
+
+    public void Call(string from, string to, string subject)
+    {
+        Console.WriteLine($"Calling from {from} to {to} with subject {subject}");
     }
 
     public void Call(string from, IEnumerable<string> tos, string subject)
@@ -17,6 +17,54 @@ public class Phone
         foreach (var to in tos)
         {
             Call(from, to, subject);
+        }
+    }
+}
+
+// Wzorzec Projektowy Budowniczny (Builder Design Pattern)
+public class FluentPhone
+{
+    private string from;
+    private ICollection<string> to = [];
+
+    private FluentPhone()
+    { 
+    }
+
+    public static FluentPhone Hangup()  // Metoda fabrykująca (Fabric Method Design Pattern)
+    {
+        return new FluentPhone();
+    }
+
+    public FluentPhone From(string from)
+    {
+        this.from = from;
+
+        return this;
+    }
+
+    public FluentPhone To(string to)
+    {
+        this.to.Add(to);
+
+        return this;
+    }
+
+    public void Call()
+    {
+        Call(from, to);
+    }
+
+    private void Call(string from, string to)
+    {
+        Console.WriteLine($"Calling from {from} to {to}");
+    }
+
+    private void Call(string from, IEnumerable<string> tos)
+    {
+        foreach (var to in tos)
+        {
+            Call(from, to);
         }
     }
 }
--- src/MethodChaining/FluentApi/Program.cs
+++ src/MethodChaining/FluentApi/Program.cs
@@ -1,2 +1,28 @@
-﻿Console.WriteLine("Hello, Method Chaining!");
+﻿using FluentApi;
+
+Console.WriteLine("Hello, Method Chaining!");
+
+//Phone phone = new Phone();
+//phone.Call("555-000-111", "555-111-222");
+
+// Fluent Api
+FluentPhone
+    .Hangup()
+    .From("555-000-111")
+    .To("555-111-222")
+    .To("555-111-333")
+    .To("555-111-444")
+    .Call();
+
+/*
+
+FluentPhone
+    .Hangup()
+    .From("555-000-111")
+    .To("555-111-222")
+    .To("555-111-333")
+    .To("555-111-444")
+    .Call();
+
+*/
 
