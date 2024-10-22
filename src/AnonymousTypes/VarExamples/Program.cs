6afd113c93628bb2b9ff0689fe8b27b8df089979
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-22 15:43:25 +0200

    Dodanie var

---
+++ src/AnonymousTypes/VarExamples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Hello, var!");
+
+var x = 10;
+
+// Typ anonimowy
+var customer = new { FirstName = "John", LastName = "Smith", Age = 18 };
+
+Console.WriteLine(customer.GetType());
+
+
+
+
