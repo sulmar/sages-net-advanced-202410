d69629d6670784d9761228a5eebbaaac483c706d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-22 08:43:20 +0200

    Dodanie przykładów Fluent

---
+++ src/MethodChaining/FluentApi/Building.cs
@@ -0,0 +1,26 @@
+﻿namespace FluentApi;
+
+public class Building
+{
+    public string Name { get; set; }
+}
+
+public class Roof
+{
+    public string Name { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+}
+
+public class Column
+{
+    public string Name { get; set; }
+    public double Height { get; }
+}
+
+public class Floor
+{
+    public string Name { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+}
