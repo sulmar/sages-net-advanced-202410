e6937eb4810b8b12768a280094cb02c8fbcd9438
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-23 09:54:38 +0200

    Dodanie przykładu rozwiązania zadania

---
+++ src/SensorsConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using SensorsConsoleApp;
+
+Console.WriteLine("Hello, Excercise 1!");
+
+var sensors = new List<Sensor>
+{
+    new TemperatureSensor { Name = "Room 1" },
+    new TemperatureSensor { Name = "Room 2" },
+
+    new HumiditySensor { Name = "Room 1" },
+    new MotionSensor { Name = "Room 2"}
+};
+
+while (true)
+{
+    foreach (var sensor in sensors)
+    {
+        sensor.UpdateValue();
+
+        Console.WriteLine(sensor);
+    }
+
+    Thread.Sleep(1000);
+}
+
---
+++ src/SensorsConsoleApp/TemperatureSensor.cs
@@ -0,0 +1,53 @@
+﻿namespace SensorsConsoleApp;
+
+public abstract class Sensor
+{
+    public string Name { get; set; }
+    public DateTime? LastUpdated { get; set; }
+    public virtual void UpdateValue()
+    {
+        LastUpdated = DateTime.UtcNow;
+    }
+}
+
+public abstract class Sensor<TValue> : Sensor
+{    
+    public TValue Value { get; set; }
+
+    protected Random random = new Random();
+   
+    public override string ToString()
+    {
+        return $"Sensor: {Name}, Value: {Value}, Last Updated: {LastUpdated}";
+    }
+}
+
+public class TemperatureSensor : Sensor<double>
+{
+    public override void UpdateValue()
+    {
+        Value = random.NextDouble() * 100;
+
+        base.UpdateValue();
+    }  
+}
+
+public class HumiditySensor : Sensor<int>
+{
+    public override void UpdateValue()
+    {
+        Value = random.Next(1, 100);
+
+        base.UpdateValue();
+    }
+}
+
+public class MotionSensor : Sensor<bool>
+{
+    public override void UpdateValue()
+    {
+        Value = random.Next(1, 100) % 2 == 0;
+
+        base.UpdateValue();
+    }
+}
