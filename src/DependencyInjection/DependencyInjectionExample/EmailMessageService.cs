7a7f0adcea47c1f5d4fe76a723da22d62ece063e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-23 16:05:21 +0200

    Dodanie interfejsów

---
+++ src/DependencyInjection/DependencyInjectionExample/EmailMessageService.cs
@@ -0,0 +1,100 @@
+﻿using System.Security.Cryptography;
+
+namespace DependencyInjectionExample;
+
+// TODO: Pokaż różnice Singleton, Scoped, Transient
+
+public interface ICustomerRepository
+{
+    void Add();
+}
+
+public class DbCustomerRepository : ICustomerRepository
+{
+    private DbContext context;
+
+    public DbCustomerRepository()
+    {
+        context = new DbContext();
+    }
+
+    public void Add()
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
+public class DbContext
+{
+
+}
+
+public class CustomersViewModel
+{
+    private ICustomerRepository customerRepository;
+    private IMessageService messageService;
+    
+    public CustomersViewModel(ICustomerRepository customerRepository, IMessageService messageService)
+    {        
+        this.customerRepository = customerRepository;
+        this.messageService = messageService;
+    }
+    
+    public string Message { get; set; }
+
+    public void Add()
+    {
+        customerRepository.Add();
+        messageService.Send(Message);
+    }
+
+    public void Update()
+    {
+        messageService.Send(Message);
+    }
+}
+
+public class CustomersController
+{
+    private ICustomerRepository customerRepository;
+    private IMessageService messageService;
+
+    public CustomersController(ICustomerRepository customerRepository, IMessageService messageService)
+    {
+        this.customerRepository = customerRepository;
+        this.messageService = messageService;
+    }
+
+    public void Post(string message)
+    {
+        customerRepository.Add();
+        messageService.Send(message);
+    }
+
+    public void Put(string message)
+    {
+        messageService.Send(message);
+    }
+}
+
+public interface IMessageService
+{
+    void Send(string message);
+}
+
+public class EmailMessageService : IMessageService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Sending {message} by email...");
+    }
+}
+
+public class SmsMessageService : IMessageService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Sending {message} by sms...");
+    }
+}
