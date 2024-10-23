3e775f8eca3ccda7122c3580c6f9a46fb02fa1de
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-23 08:31:46 +0200

    Dodanie przykładów Thread i ThreadPool

---
+++ src/Threading/ThreadExample/ThreadHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace ThreadExample;
+
+public static class ThreadHelper
+{
+    public static void DumpThreadId(this string message)
+    {
+        Console.WriteLine($"Thread ID = {Thread.CurrentThread.ManagedThreadId} : {message}");
+    }
+}
---
+++ src/Threading/ThreadPoolExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Net;
+using ThreadPoolExample;
+
+Console.WriteLine("Hello, Thread Pool!");
+Console.WriteLine("Hello, Thread!");
+
+const string defaultUri = "https://picsum.photos/800/600";
+const int count = 100;
+
+// Wygenerowanie listy adresów url
+string[] urls = Enumerable.Range(0, count).Select(_ => defaultUri).ToArray();
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+foreach (string url in urls)
+{
+    Download(url);
+}
+
+stopwatch.Stop();
+
+Console.WriteLine("All downloads completed.");
+
+Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
+
+
+static void Download(string url)
+{
+    using (var client = new WebClient())
+    {
+        $"Downloading {url}...".DumpThreadId();
+
+        // Symulacja opóźnienia
+        Thread.Sleep(Random.Shared.Next(200, 1000));
+
+        $"Downoladed. {url}".DumpThreadId();
+    }
+}
