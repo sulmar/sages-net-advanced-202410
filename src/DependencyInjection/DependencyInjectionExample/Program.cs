22ac8f9c15ed7a8979d752e6cbed2cbc8469e32d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-23 16:30:02 +0200

    Dodanie wstrzykiwania zależności

--- src/DependencyInjection/DependencyInjectionExample/Program.cs
+++ src/DependencyInjection/DependencyInjectionExample/Program.cs
@@ -1,17 +1,23 @@
 ﻿using DependencyInjectionExample;
+using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Hello, Dependency Injection!");
 
 // dotnet add package Microsoft.Extensions.DependencyInjection
 
-CustomersViewModel customersViewModel = new CustomersViewModel(
-                    new DbCustomerRepository(new DbContext()),
-                            new SmsMessageService());
+// Tworzymy kontener do wstrzykiwania zależności (Dependency Injection)
 
+var serviceProvider = new ServiceCollection()
+    .AddTransient<IMessageService, EmailMessageService>()
+    .AddTransient<ICustomerRepository, DbCustomerRepository>()
+    .AddTransient<DbContext>()
+    .AddTransient<CustomersViewModel>()
+    .AddTransient<CustomersController>()
+    .BuildServiceProvider();
 
-CustomersController customersController = new CustomersController(
-                        new DbCustomerRepository(new DbContext()),
-                            new SmsMessageService());
+CustomersViewModel customersViewModel = serviceProvider.GetService<CustomersViewModel>();
+
+CustomersController customersController = serviceProvider.GetService<CustomersController>();
 
 
 customersViewModel.Add();
