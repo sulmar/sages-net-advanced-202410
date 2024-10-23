ca5e9018e89d93b84ebb1e956da5abcabd034e31
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-23 17:05:07 +0200

    Fix: Zmiana nazwy na id

--- src/GenericTypes/GenericClassses/Program.cs
+++ src/GenericTypes/GenericClassses/Program.cs
@@ -4,22 +4,22 @@ Console.WriteLine("Hello, Generic Classes!");
 
 var customers = new List<Customer>
 {
-    new Customer { CustomerId = 1, Name = "Microsoft" },
-    new Customer { CustomerId = 2, Name = "Google" },
-    new Customer { CustomerId = 3, Name = "Apple" },
+    new Customer { Id = 1, Name = "Microsoft" },
+    new Customer { Id = 2, Name = "Google" },
+    new Customer { Id = 3, Name = "Apple" },
 };
 
 var accounts = new List<Account>
 {
-    new Account { AccountId = 1, Number = "11110000", Balance = 100},
-    new Account { AccountId = 2, Number = "22220000", Balance = 200 },
-    new Account { AccountId = 3, Number = "33330000", Balance = 300 },
-    new Account { AccountId = 4, Number = "44440000", Balance = 400 },
-    new Account { AccountId = 5, Number = "66660000", Balance = 500 },
+    new Account { Id = 1, Number = "11110000", Balance = 100},
+    new Account { Id = 2, Number = "22220000", Balance = 200 },
+    new Account { Id = 3, Number = "33330000", Balance = 300 },
+    new Account { Id = 4, Number = "44440000", Balance = 400 },
+    new Account { Id = 5, Number = "66660000", Balance = 500 },
 };
 
 InMemoryCustomerRepository repository = new InMemoryCustomerRepository(customers);
 
-var customer = repository.GetCustomer(1);
+var customer = repository.Get(1);
 
 Console.WriteLine(customer);
