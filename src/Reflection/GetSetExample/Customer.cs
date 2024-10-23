593d3b2c975e609590d4859b71b7aaf67b41995c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-23 11:30:28 +0200

    Pobieranie i ustawianie wartości za pomocą refleksji

--- src/Reflection/GetSetExample/Customer.cs
+++ src/Reflection/GetSetExample/Customer.cs
@@ -1,8 +1,46 @@
-﻿namespace GetSetExample;
+﻿using System.Net.NetworkInformation;
+using System.Reflection;
+
+namespace GetSetExample;
 
 public class Customer
 {    
     public string Name { get; set; }
     public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name = {Name}, Email = {Email}";
+    }
+
+    public void DoWork()
+    {
+        throw new NotImplementedException();
+        Console.WriteLine(Name);
+    }
+
+    // Własny indekser
+    public object this[string propertyName]
+    {
+        get
+        {
+            Type type = this.GetType();
+
+            PropertyInfo property = type.GetProperty(propertyName);
+
+            object value = property.GetValue(this);
+
+            return value;
+        }
+
+        set
+        {
+            Type type = this.GetType();
+
+            PropertyInfo property = type.GetProperty(propertyName);
+
+            property.SetValue(this, value);
+        }
+    }
 
 }
--- src/Reflection/GetSetExample/Program.cs
+++ src/Reflection/GetSetExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GetSetExample;
+using System.Reflection;
 
 Console.WriteLine("Hello, Reflection!");
 
@@ -8,3 +9,28 @@ Customer customer = new Customer { Name = "John Doe", Email = "john.doe@example.
 // TODO:  Przypisz wartości do właściwości za pomocą indeksu
 // customer["Name"] = "John Doe";
 // customer["Email"] = "john.doe@example.com";
+
+Console.Write("Poda nazwę właściwości (Name/Email): ");
+string propertyName = Console.ReadLine();
+
+Type type = customer.GetType();
+
+Console.WriteLine($"{type.Namespace} {type.Name}");
+
+PropertyInfo property = type.GetProperty(propertyName);
+
+// Odczyt wartości
+object value = property.GetValue(customer);
+
+Console.WriteLine(value);
+
+// Ustawienie wartości
+property.SetValue(customer, "John Smith");
+
+Console.WriteLine(customer);
+
+customer[propertyName] = "Marcin";
+
+Console.WriteLine(customer[propertyName]);
+
+Console.WriteLine(customer);
