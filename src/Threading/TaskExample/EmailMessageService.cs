a1e4db908c56f8485b01d484f9f6bd8da36eff62
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-24 12:45:20 +0200

    Dodanie Task rownoleglych

---
+++ src/Threading/TaskExample/EmailMessageService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExample;
+
+public class EmailMessageService
+{
+    private Random random = new Random();
+
+    public void SendToMe()
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        $"Sending...".DumpThreadId();
+
+        Thread.Sleep(random.Next(1000, 3000));
+
+        stopwatch.Stop();
+
+        $"Sent. Elapsed time: {stopwatch.Elapsed}".DumpThreadId();
+    }
+
+    public void SendTo(string to)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        $"Sending to {to}...".DumpThreadId();
+
+        Thread.Sleep(random.Next(1000, 3000));
+
+        stopwatch.Stop();
+
+        $"Sent to {to}. Elapsed time: {stopwatch.Elapsed}".DumpThreadId();
+    }
+}
