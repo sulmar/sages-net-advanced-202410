624b8662eeb9fe13c89312f46a894e00bace0882
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-24 10:16:48 +0200

    Dodanie przykładu

---
+++ src/ControlAirConditionerApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using ControlAirConditionerApp;
+
+Console.WriteLine("Hello, World!");
+
+TemperatureSensor temperatureSensor = new TemperatureSensor();
+temperatureSensor.HighTemperature += OnHighTemperature;
+temperatureSensor.HighTemperature += (temperature) =>
+{
+    Fan fan = new Fan();
+    fan.TurnOff();
+};
+
+temperatureSensor.LowTemperature += OnLowTemperature;
+temperatureSensor.LowTemperature += (temperature) =>
+{
+    Fan fan = new Fan();
+    fan.TurnOn();
+};
+
+while (true)
+{
+    Console.Write("Podaj temperaturę: ");
+    double temperature = double.Parse(Console.ReadLine());
+
+    temperatureSensor.ReadTemperature(temperature);
+
+}
+
+
+static void OnHighTemperature(double temperature)
+{
+    AirConditioner ac = new AirConditioner();    
+
+    ac.TurnOn();
+}
+
+static void OnLowTemperature(double temperature)
+{
+    AirConditioner ac = new AirConditioner();
+
+    ac.TurnOff();
+}
---
+++ src/ControlAirConditionerApp/TemperatureSensor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlAirConditionerApp;
+
+// Klasa czujnika temperatury
+public class TemperatureSensor
+{
+    public delegate void LimitTemperatureDelegate(double temperature);
+    
+    public LimitTemperatureDelegate HighTemperature { get; set; }
+    public LimitTemperatureDelegate LowTemperature { get; set; }
+
+    // Metoda symulująca odczyt z czujnika
+    public void ReadTemperature(double temperature)
+    {
+        Console.WriteLine($"Odczytana temperatura: {temperature}°C");
+
+        if (temperature > 25)       // Magic Number
+        {
+            HighTemperature?.Invoke(temperature);         
+        }
+        else
+        {
+            LowTemperature?.Invoke(temperature);
+        }
+    }
+}
+
+// Klasa klimatyzacji
+public class AirConditioner
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Klimatyzacja włączona.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Klimatyzacja wyłączona.");
+    }
+}
+
+// Klasa wentylatora
+public class Fan
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Wentylator włączony.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Wentylator wyłączony.");
+    }
+}
+
+
