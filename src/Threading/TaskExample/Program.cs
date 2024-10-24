a92bacc54944da3c1ec109f8f2e98b7ebfa50a02
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-24 12:55:36 +0200

    Dodanie ContinueWith

--- src/Threading/TaskExample/Program.cs
+++ src/Threading/TaskExample/Program.cs
@@ -1,17 +1,50 @@
-﻿using TaskExample;
+﻿using System.Net.Http.Headers;
+using TaskExample;
 
 Console.WriteLine("Hello, Tasks!");
 
+EmailMessageService messageService = new EmailMessageService();
 
-Console.WriteLine("Starting payroll calculation...");
+Task.Run(()=> messageService.SendTo("john@domain.com"))
+    .ContinueWith(t => messageService.SendTo("kate@domain.com"));
 
-decimal hourlyRate = 50m;
-int hoursWorked = 160;
+Console.WriteLine("Press any to exit.");
+Console.ReadKey();
 
-SalaryCalculator calculator  = new SalaryCalculator();
 
-decimal grossSalary = calculator.CalculateGrossSalary(hourlyRate, hoursWorked);
 
-decimal tax = calculator.CalculateTax(grossSalary);
+void SendTest()
+{
+    EmailMessageService messageService = new EmailMessageService();
+  
 
-Console.WriteLine($"Tax: {tax:C}");
+    "Started.".DumpThreadId();
+
+    IEnumerable<Task> tasks = Enumerable.Range(1, 100).Select(_ => new Task(() => messageService.SendToMe()));
+
+    foreach (Task task in tasks)
+    {
+        task.Start();
+    }
+
+    "Done.".DumpThreadId();
+
+}
+
+// TaskWithResultTest();
+
+static void TaskWithResultTest()
+{
+    Console.WriteLine("Starting payroll calculation...");
+
+    decimal hourlyRate = 50m;
+    int hoursWorked = 160;
+
+    SalaryCalculator calculator = new SalaryCalculator();
+
+    decimal grossSalary = calculator.CalculateGrossSalary(hourlyRate, hoursWorked);
+
+    decimal tax = calculator.CalculateTax(grossSalary);
+
+    Console.WriteLine($"Tax: {tax:C}");
+}
